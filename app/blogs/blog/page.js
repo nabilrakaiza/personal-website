e36b117a0c3d9c79dbcc37968208091a300a76e2
nabilrakaiza <nabilraka1234@gmail.com>
2025-11-29 14:45:10 +0800

    add: update blog

--- app/blogs/blog/page.js
+++ app/blogs/blog/page.js
@@ -2,90 +2,197 @@ import React from "react";
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from "remark-gfm";
 
-const rawContent = `Lecturer: Prof. Vik Gopal
+export default function BlogPage({searchParams}) {
+  const id = searchParams.id;
+  const content = blogContents[id]["content"]
+  const title = blogContents[id]["title"]
 
-### Assessment
-- 10% DataCamp assignments
-- 30% Tutorials (each 2% except the 7th which is 10%)
-- 20% Midterm
-- 40% Finals
+  return (
+    <div className="bg-gray-900 min-h-screen text-white font-sans">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
 
-### Module Difficulty
-The module difficulty actually depends on whether you know R well or not. It is very useful to understand how to read R help pages and the main star of this module, dplyr and ggplot2.
+        <p className="text-center text-white mb-8 text-2xl"><b>{title}</b></p>
+          {/* Main Content */}
+          <main className="py-3">
+              <div className="bg-amber-300 rounded-4xl p-6 transition-shadow text-black">
+                  <ReactMarkdown remarkPlugins={remarkGfm} components={{
+  p: ({ node, ...props }) => <p className="mb-4" {...props} />,
+  li: ({ node, ...props }) => <li className="ml-6 list-disc" {...props} />,
+  h3: ({ node, ...props }) => <h3 className="text-xl font-semibold mt-6 mb-2" {...props} />,
+}}>
+                      {content} 
+                  </ReactMarkdown>
+              </div>
+          </main>
 
-For the first few weeks you are refreshed upon the basic R commands, and how you should try to program functionally instead of declaratively, i.e. use the apply command family such as sapply and lapply instead of the well-known control flows like for and while or even repeat.
-
-Next up is about importing data, where it can be from an HTML page, some API, excel files, you name it. This just opens our mind to what kinds of data storage are there out in the wild. You will also touch the concept of simple feature objects (sf) which is useful for spatial data.
-
-Starting the middle of the semester we were introduced to dplyr which is very convenient to use when manipulating data. It feels like method chaining at its finest with the help of the pipe operator.
-
-With all data importing and manipulating skills in hand, it's time to output and visualize the data as the module title says, which is using ggplot2. There are many ways of visualizing the same data, and we also went through the rationale on visualizing different types of data, such as considering the data-ink ratio, the accuracy of the data, and how the visualization is approachable to layman people.
-
-As a supplementary material but not examinable, we were also introduced to Tableau on the last week, and how the basic tools work.
-
-### Workload (Weekly)
-**Lectures - 4h (2 x 2h)**
-
-Prof decided to use Microsoft Teams as the main communication platform, which I had no complain about. He will definitely provide the recording, but his lectures are just so calming I'd actually come for it. He also touched on some extra information such as some statistical methods that might be useful for further modules. It's a good idea to graze on these so as to give us awareness of their existence, e.g. how does the loess method work, instead of just using it in lm's method. He also has a website that basically displays his code live, and we can comment on the code that he's currently explaining or copying them for easy reference and learning. It's just such easy to work around with these.
-
-**Tutorials - 1h**
-
-Basically, for every week, there will be a tutorial worksheet released to be done in a week by submitting Rmd files (R markdown). As someone who's been familiar with GitHub Markdown files, I had no trouble familiarizing myself with Rmd files, but learning this becomes crucial when you have to always submit everything in this file format. The one-hour session is to simply discuss about these problems for you as a help to solve these questions. It is also recorded. Personally, the questions take a while to solve (at least 3 hours?) but I enjoy exploring these datasets provided beforehand. There's actually more insights you can gain the moment you explore them.
-
-**DataCamp assignments**
-
-Prof used DataCamp for non-tutorial submissions which basically is an extra guide on how to work with everything taught in DSA2101. It is a comfortable tool and you don't have to worry about being wrong here because you are just graded for the sake of completeness. Also very chiong-able, how about finishing all ten in the end of Week 0 :)
-
-### Exams and Personal Opinion
-Hands up the mod I had the most fun during the semester. Had ups and downs during debugging but isn't that the way of life.
-
-Midterms was a one-day take home thing. So, you can ask for clarification during the next 24h of the paper's release. Difficulty-wise, it's medium difficulty and definitely can be searched by R's help page as well.
-
-Finals was held F2F and the difficulty was easier than I expected, thankfully. There was a moment where I realized understanding the help page in a short amount time is a useful thing, since there was one package rarely used I only came upon it during exam yet I had to use it.
-
-Overall, I learnt a lot from this module and how do we deal different kinds of datasets as a way of adapting.
-
-**Expected grade:** A`;
-
-const NavLink = ({ href, children, className = "px-4 py-2 rounded-full text-sm font-medium transition-colors", active = false }) => (
-    <a
-      href={href}
-      className={`${className} ${
-        active
-          ? 'bg-gray-700 text-white'
-          : 'text-gray-400 hover:text-white'
-      }`}
-    >
-      {children}
-    </a>
-  );
-  
-  export default function BlogPage() {
-    return (
-      <div className="bg-gray-900 min-h-screen text-white font-sans">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-
-          <p className="text-center text-white mb-8 text-2xl"><b>DSA2101: Essential Data Analytics Tools: Data Visualisation </b></p>
-            {/* Main Content */}
-            <main className="py-3">
-                <div className="bg-amber-300 rounded-4xl p-6 transition-shadow text-black">
-                    <ReactMarkdown remarkPlugins={remarkGfm} components={{
-    p: ({ node, ...props }) => <p className="mb-4" {...props} />,
-    li: ({ node, ...props }) => <li className="ml-6 list-disc" {...props} />,
-    h3: ({ node, ...props }) => <h3 className="text-xl font-semibold mt-6 mb-2" {...props} />,
-  }}>
-                        {rawContent}
-                    </ReactMarkdown>
-                </div>
-            </main>
-  
-          {/* Footer */}
-          <footer className="text-center text-gray-500 py-8">
-            <p>&copy; 2025 Nabil Rakaiza Abror.</p>
-          </footer>
-        </div>
+        {/* Footer */}
+        <footer className="text-center text-gray-500 py-8">
+          <p>&copy; 2025 Nabil Rakaiza Abror.</p>
+        </footer>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+
+const blogContents = {
+  "y2s1-review" : {
+    "title": "Y2S1 Module Review",
+    "content": `CS2040
+
+I like this module a lot, even though it might be difficult for some of you. I enjoy solving the weekly tutorial and doing the take home assignments as it challenges me to solve the problem without much external help. I believed my score was near high until finals came (i’m not really sure with my finals). 
+
+Expected Grade: A-/A
+
+----------------------------------------------------------------    
+
+CS2030
+
+For the first half of this module, I was doing just fine. But suddenly, somehow, I managed to throw everything. The module was taught great, but I think it was more about me who doesn’t really like the course itself (I prefer to do algo problems than designing pipelines). 
+
+Expected Grade: B/B+
+
+----------------------------------------------------------------    
+
+DSA2101
+
+Even though most of the time you spent on this module is plotting graph, I find it enjoyable, especially the group project. I was able to understand how a good graph is easier for humans to understand and how we construct it to answer questions. Midterm and finals were fine (I think), but the bell curve is known to be very steep. 
+
+Expected Grade: A-
+
+----------------------------------------------------------------    
+
+DSA2102
+
+I love this module so much, especially the first half of the module. You will learn how computers do calculations and what their limitations are. In the end, I think I should be able to score well since I got near high for the midterm and feel confident for finals. 
+
+Expected Grade: A
+
+----------------------------------------------------------------    
+
+IT1244
+
+I also love this module. Some of the algorithms are actually taught in other courses, like DSA1101 (KNN, Kmeans, Linear Regression, Logistic Regression, etc) and CS2040 (DFS, BFS). So I was able to grasp the content quite quickly, and I think I can score well for this course. Some tips I want to share are: pls take this module with your friends if you don’t want teammates who aren't good enough. 
+
+Expected Grade: A-/A
+
+----------------------------------------------------------------    
+
+SP1541
+
+Well, I have a love-hate relationship with this mod. There are no statistics at all, so I can’t compare myself with the other cohorts. Other than that, the grading is quite subjective, so you can get a poor grade if you are unlucky. I also somehow never got > 70 for any assessment other than a quiz (which is basically also a free mark for everyone). But, the class activities were fun, and I like going to the science centre also. 
+
+Expected Grade: C (S)
+`
+  },
   
-  
+  "y1s2-review" : {
+    "title": "Y1S2 Module Review",
+    "content": `
+    CS1231
+
+I find this module to be enjoyable as I can brush up my knowledge on discrete math again. However, the grading of this module is purely based on assessment, so please do your assessment carefully. 
+
+Final Grade: A-
+
+----------------------------------------------------------------
+
+DSA1101
+
+This module was very interesting as you learn how several machine learning algorithms work. The tutorial session was fun as well, thanks to my wonderful TA. However, I don’t really like the usage of R in this course, since most of the time python is the best language for developing machine learning algorithms. 
+
+Final Grade: A+
+
+----------------------------------------------------------------
+
+ST2131
+
+This module was hard as hell. For the final exam, I believe I can only do 2-3 questions, and I just wrote gibberish for the rest of the questions. But I find the material to be interesting, and I am still very curious about the answer to the final exam to this day. 
+
+Final Grade: A-
+
+----------------------------------------------------------------
+
+ES1103
+
+So, basically, this module will teach you how to write an academic text. You will be given a task to create around 1000 words for the entire semester, and it will be divided into several CAs. I think, as long as you consult a lot with the tutor, you should be able to get a good grade already
+
+Final Grade: A- 
+
+----------------------------------------------------------------
+
+HSS1000
+
+Another CHS fluff mods. Nothing much to say since I’m planning to SU since the very beginning.
+
+Final Grade: S 
+
+----------------------------------------------------------------
+
+HSA1000
+
+Another one CHS fluff mods. Nothing much to say since I’m planning to SU since the very beginning.
+
+Final Grade: S
+
+----------------------------------------------------------------
+
+DTK1234
+
+Another one CHS fluff mods. Nothing much to say since I’m planning to SU since the very beginning.
+
+Final Grade: S
+`
+  },
+
+  "y1s1-review" : {
+    "title": "Y1S1 Module Review",
+    "content": `
+    CS1010S
+
+I feel very confident for this module since I have a background in python. However, if you don’t have any background before, this mod might be very hard for you, and you might want to take this with easier mods. 
+
+Final Grade: A+
+
+----------------------------------------------------------------
+
+MA2001
+
+This module wasn’t quite hard, it’s just that the work might be long and tedious. As long as you didn’t mess up your calculation and practice a lot, you might be able to get a good grade. 
+
+Final Grade: A
+
+----------------------------------------------------------------
+
+MA2002
+
+This module was a bit hard, especially the final exam. Maybe it’s just me that are not prepared enough for this exam, but I legit only did like ⅔ of the exam itself. However, I think others are also find it hard as well since my final grade wasn’t so bad. 
+
+Final Grade: A- 
+
+----------------------------------------------------------------
+
+HSI1000
+
+This module was fun as there is a workshops where you actually go to real lab and do some science stuff. However, the assessment was quite boring and you can just bring a lot of cheatsheets (pdf) and do ctrl+f for the entire exam. 
+
+Final Grade: A-
+
+----------------------------------------------------------------
+
+HSH1000
+
+This module was fun for me, but not so much when it comes to grading. I always scored under lower quartile for every quiz since it was hard for me. Maybe this module just wasn’t for me, but the discussion in class is fun though!
+
+Final Grade: S
+
+----------------------------------------------------------------
+
+CFG1002
+
+This is just a career preparation mod that you can finish everything in 1 day (with 2 live lecture that you need to come).
+
+Final Grade: S
+`
+  },
+}
--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,75 +1,52 @@
-// import { useState, useEffect } from 'react';
-
-const NavLink = ({ href, children, className = "px-4 py-2 rounded-full text-sm font-medium transition-colors", active = false }) => (
-  <a
-    href={href}
-    className={`${className} ${
-      active
-        ? 'bg-gray-700 text-white'
-        : 'text-gray-400 hover:text-white'
-    }`}
-  >
-    {children}
-  </a>
-);
-  
-  export default function BlogsPage() {
-    return (
-      <div className="bg-gray-900 min-h-screen text-white font-sans">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-          <p className="text-center text-white mb-8 text-2xl"><b>My blogs</b> (<i>still a template, cause I dont write that much</i>)</p>
-            {/* Main Content */}
-            <main className="py-3">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {/* Example Blog Card */}
-                    <div className="bg-amber-300 p-6 rounded-4xl hover:shadow-lg transition-shadow">
-                        <h3 className="text-xl font-semibold mb-2 text-black">Y1S2 Module Review!</h3>
-                        <h3 className="mb-2 text-gray-800 italic">April 2025</h3>
-                        <p className="text-black mb-4">So basicaly I took 7 mods this sem, which are DSA1101, CS1231, ST2131, HSS1000, HSA1000
-                        DTK1234, and ES1103. For my core mode, the grade is a little better than expected since I bombed the finals, but I will
-                        still accept it! Then for the rest of the mods, I honestly doesn&rsquo;t really care on the grade that I will get since I have 5 SU 
-                        left to use.</p>
-                        <p className="text-black hover:text-gray-500">Read more...</p>
-                    </div>
-
-                    <div className="bg-amber-300 p-6 rounded-4xl hover:shadow-lg transition-shadow">
-                        <h3 className="text-xl font-semibold mb-2 text-black">Y1S1 Module Review!</h3>
-                        <h3 className="mb-2 text-gray-800 italic">December 2024</h3>
-                        <p className="text-black mb-4">So basicaly I took 7 mods this sem, which are DSA1101, CS1231, ST2131, HSS1000, HSA1000
-                        DTK1234, and ES1103. For my core mode, the grade is a little better than expected since I bombed the finals, but I will
-                        still accept it! Then for the rest of the mods, I honestly doesn&rsquo;t really care on the grade that I will get since I have 5 SU 
-                        left to use.</p>
-                        <p className="text-black hover:text-gray-500">Read more...</p>
-                    </div>
-
-                    <div className="bg-amber-300 p-6 rounded-4xl hover:shadow-lg transition-shadow">
-                        <h3 className="text-xl font-semibold mb-2 text-black">Y2S1 Module Review!</h3>
-                        <h3 className="mb-2 text-gray-800 italic">December 2025</h3>
-                        <p className="text-black mb-4">So basicaly I took 7 mods this sem, which are DSA1101, CS1231, ST2131, HSS1000, HSA1000
-                        DTK1234, and ES1103. For my core mode, the grade is a little better than expected since I bombed the finals, but I will
-                        still accept it! Then for the rest of the mods, who cares.</p>
-                        <p className="text-black hover:text-gray-500">Read more...</p>
-                    </div>
-
-                    <div className="bg-amber-300 p-6 rounded-4xl hover:shadow-lg transition-shadow">
-                        <h3 className="text-xl font-semibold mb-2 text-black">A little something about myself</h3>
-                        <h3 className="mb-2 text-gray-800 italic">September 2025</h3>
-                        <p className="text-black mb-4">So basicaly I took 7 mods this sem, which are DSA1101, CS1231, ST2131, HSS1000, HSA1000
-                        DTK1234, and ES1103. For my core mode, the grade is a little better than expected since I bombed the finals, but I will
-                        still accept it! Then for the rest of the mods, I honestly doesn&rsquo;t really care on the grade that I will get since I have 5 SU 
-                        left to use.</p>
-                        <p className="text-black hover:text-gray-500">Read more...</p>
-                    </div>
-                </div>
-            </main>
-  
-          {/* Footer */}
-          <footer className="text-center text-gray-500 py-8">
-            <p>&copy; 2025 Nabil Rakaiza Abror.</p>
-          </footer>
-        </div>
-      </div>
-    );
+export const blogPosts = [
+  {
+    id: "y2s1-review",
+    title: "Y2S1 Module Review!",
+    date: "December 2025",
+    summary: "In this sem, I took CS2030, CS2040, IT1244, SP1541, DSA2101, DSA2102. Click to read more about my review!",
+  },
+  {
+    id: "y1s2-review",
+    title: "Y1S2 Module Review!",
+    date: "May 2025",
+    summary: "In this sem, I took DSA1101, ST2131, CS1231, HSA1000, HSS1000, DTK1234, ES1103. Click to read more about my review!",
+  },
+  {
+    id: "y1s1-review",
+    title: "Y1S1 Module Review!",
+    date: "December 2024",
+    summary: "In this sem, I took CS1010S, MA2001, MA2002, HSH1000, HSI1000, CFG1002. Click to read more about my review!",
+    content: "Here is the full long text for Y1S1..."
   }
-  
-  
+];
+
+export default function BlogsPage() {
+  return (
+    <div className="bg-gray-900 min-h-screen text-white font-sans flex flex-col">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex-1 flex flex-col">
+        
+        <p className="text-center text-white mb-8 text-2xl mt-8"><b>My blogs</b></p>
+        <main className="py-3 flex-1">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+
+              {blogPosts.map((post) => (
+                <a href={`/blogs/blog?id=${post.id}`} key={post.id}>
+                  <div className="bg-amber-300 p-6 rounded-4xl hover:shadow-lg transition-shadow cursor-pointer h-full">
+                      <h3 className="text-xl font-semibold mb-2 text-black">{post.title}</h3>
+                      <h3 className="mb-2 text-gray-800 italic">{post.date}</h3>
+                      <p className="text-black mb-4">{post.summary}</p>
+                  </div>
+                </a>
+              ))}
+
+            </div>
+        </main>
+
+        {/* Footer */}
+        <footer className="text-center text-gray-500 pb-8">
+          <p>&copy; 2025 Nabil Rakaiza Abror.</p>
+        </footer>
+      </div>
+    </div>
+  );
+}
