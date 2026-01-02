111284a4725099603e834bdf4df1557a882165c5
nabilrakaiza <nabilraka1234@gmail.com>
2026-01-02 08:13:51 +0700

    update: adding analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/ui/Header";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
       >
         <Header /> 
         {children}
+        <Analytics />
       </body>
     </html>
   );
